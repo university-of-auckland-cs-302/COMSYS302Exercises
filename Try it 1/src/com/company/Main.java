c14178199d5286c8ee807255d5587fbe025a6980
Reza Shahamiri <61479740+rshahamiriuoa@users.noreply.github.com>
2020-02-26 15:37:11 +1300

    Null

---
+++ Try it 1/src/com/company/Main.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a, b, c;
+        a = 1234567899;
+        b = 1234567899;
+        c =  a * b;
+        System.out.print(c);
+    }
+}
