8403137a2aa8f860116b07febf2d446a6c847022
Reza Shahamiri <61479740+rshahamiriuoa@users.noreply.github.com>
2020-02-26 15:01:48 +1300

    Initial commit

---
+++ Your First Java Project/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Hello class!");
+        System.out.println("This is your first Java application, I suppose?");
+        System.out.print("Happy learning :-)");
+    }
+}
