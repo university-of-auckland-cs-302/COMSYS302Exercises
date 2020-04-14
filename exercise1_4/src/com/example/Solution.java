5677e81c6b9714292535e09a0962f14a1659b8ac
Yuanyuan Hu <61485612+yuanhu19@users.noreply.github.com>
2020-04-14 17:17:50 +1200

    exercise_1_4 sample code

---
+++ exercise1_4/src/com/example/Solution.java
@@ -0,0 +1,33 @@
+package com.example;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+
+        //example code here
+
+        for(int i = 1; i <= 10; i ++){
+            System.out.println(N+" x "+i+" = "+N*i);
+
+        }
+
+
+
+    }
+}
