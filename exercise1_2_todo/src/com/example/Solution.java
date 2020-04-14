bc348ff5c824386b03ae9021ee3ed4afd36a44ee
Yuanyuan Hu <61485612+yuanhu19@users.noreply.github.com>
2020-04-14 17:15:52 +1200

    exercise1_2_todo

---
+++ exercise1_2_todo/src/com/example/Solution.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int i = scan.nextInt();
+        // Write your code here.
+
+
+
+
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
