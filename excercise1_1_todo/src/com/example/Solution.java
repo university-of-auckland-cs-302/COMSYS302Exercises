45ac25db62c5a3ccf649295880824d9618ea65e0
Yuanyuan Hu <61485612+yuanhu19@users.noreply.github.com>
2020-04-14 17:12:54 +1200

    exercise1_1_todo

---
+++ excercise1_1_todo/src/com/example/Solution.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+
+        System.out.println(a);
+        // Complete this line
+        // Complete this line
+
+    }
+}
