ce7aa02ab73d00de530d6d8901a306594a35c2ec
Yuanyuan Hu <61485612+yuanhu19@users.noreply.github.com>
2020-04-14 17:15:05 +1200

    exercise1_2

---
+++ exercise1_2/src/com/example/Solution.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int i = scan.nextInt();
+
+        // the example code here
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        scan.close();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
