8cf8c64952925c924a66bd1a1bd3c621c49c42a6
Yuanyuan Hu <61485612+yuanhu19@users.noreply.github.com>
2020-04-14 17:12:11 +1200

    exercise1_1

---
+++ excercise1_1/src/com/example/Solution.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+
+        // example codes here
+        int b = scan.nextInt();
+
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        // example codes here
+
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
