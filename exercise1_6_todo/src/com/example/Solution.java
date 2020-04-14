c3895570a839adb0a066b937bcff9e5b1fc6bf9e
Yuanyuan Hu <61485612+yuanhu19@users.noreply.github.com>
2020-04-14 17:20:32 +1200

    exercise1_6 to do

---
+++ exercise1_6_todo/src/com/example/Solution.java
@@ -0,0 +1,21 @@
+package com.example;
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+
+
+
+
+
+
+
+    }
+}
