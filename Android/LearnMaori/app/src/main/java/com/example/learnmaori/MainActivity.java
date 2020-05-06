37b43a9ceb23f6dfd9a11a5cd37de7619169731d
Reza Shahamiri <rshahamiri@gmail.com>
2020-05-06 16:37:47 +1200

    Learn Maori - end of lesson 12

--- Android/LearnMaori/app/src/main/java/com/example/learnmaori/MainActivity.java
+++ Android/LearnMaori/app/src/main/java/com/example/learnmaori/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,10 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CardView numbersCardView = (CardView) findViewById(R.id.card_view_numbers);
+        numbersCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent numbersIntent = new Intent(getBaseContext(), NumbersActivity.class);
+                numbersIntent.putExtra("MessageFromMainActivity", "This message came form Main Activity");
+                startActivity(numbersIntent);
+            }
+        });
     }
 
-    public void showNumbersActivity(View v) {
-        Intent numbersIntent = new Intent(this, NumbersActivity.class);
-        startActivity(numbersIntent);
-    }
 }
--- Android/LearnMaori/app/src/main/java/com/example/learnmaori/NumbersActivity.java
+++ Android/LearnMaori/app/src/main/java/com/example/learnmaori/NumbersActivity.java
@@ -1,8 +1,10 @@
 package com.example.learnmaori;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        Intent thisIntent = getIntent();
+        String message = thisIntent.getStringExtra("MessageFromMainActivity");
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
     }
 }
