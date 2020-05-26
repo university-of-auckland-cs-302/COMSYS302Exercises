c891e0eb203dd697ed00df1ed7fa9ad4ee21062d
Reza Shahamiri <rshahamiri@gmail.com>
2020-05-26 12:28:57 +1200

    Learn Maori App Lesson 13 Up to slide 13

--- Android/LearnMaori/app/src/main/java/com/example/learnmaori/NumbersActivity.java
+++ Android/LearnMaori/app/src/main/java/com/example/learnmaori/NumbersActivity.java
@@ -1,10 +1,13 @@
 package com.example.learnmaori;
 
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 
 public class NumbersActivity extends AppCompatActivity {
@@ -15,13 +18,13 @@ public class NumbersActivity extends AppCompatActivity {
         setContentView(R.layout.activity_numbers);
 
         //Testing the dictionary
-        Map<Integer, String> words = DataProvider.generateMaoriDigits();
-        String text = "";
-        for (Integer key : words.keySet()) {
-            text += String.valueOf(key) + " : " + words.get(key) + "\n";
-        }
-        TextView tempView = (TextView) findViewById(R.id.temp_text_view);
-        tempView.setText(text);
+        Map<Integer, String> dataDict = DataProvider.generateMaoriDigits();
+
+        List<String> maoriWords = new ArrayList<String>(dataDict.values());
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                maoriWords);
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listView.setAdapter(itemsAdapter);
 
 
     }
