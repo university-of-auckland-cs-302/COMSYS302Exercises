8d38010cb0f2afa0cd814f98cd63fa6644249f06
Yuanyuan Hu <61485612+yuanhu19@users.noreply.github.com>
2021-05-03 16:12:37 +1200

    exercise2_3 update

---
+++ exercise2_3/src/com/example/Book.java
@@ -0,0 +1,14 @@
+package com.example;
+
+abstract class Book{
+
+    //attribute
+    String title;
+
+    abstract void setTitle(String s);
+
+
+    String getTitle(){
+        return title;
+    }
+}
---
+++ exercise2_3/src/com/example/MyBook.java
@@ -0,0 +1,9 @@
+package com.example;
+
+class MyBook extends Book {
+
+    void setTitle(String s){
+        super.title = s;
+    }
+
+}
